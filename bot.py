865ac5fc557a798eb2d96aeb5390bd8f245a4d31
Anand Krishna Sunil <anandsunil578@gmail.com>
2019-02-17 18:20:37 +0530

    Initial Bot structure

---
+++ bot.py
@@ -0,0 +1,11 @@
+from bottle import run, post, request as bottle_request
+
+@post('/')
+def main():
+    data = bottle_request.json
+    print(data)
+    
+    return
+
+if __name__ == '__main__':
+    run(host='localhost', port=8080, debug=True)
